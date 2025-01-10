c45c64113fd5138efd9068176574238e6c57af87
Wairioko <charlesmungai5@gmail.com>
2025-01-10 16:45:11 +0300

    change of routes to confirm subscription from paddle

--- src/Subscription/routes/subscriptionRoutes.mjs
+++ src/Subscription/routes/subscriptionRoutes.mjs
@@ -1,21 +1,15 @@
 import express from 'express';
-import { SubscriptionController } from '../controller/subscriptionController.mjs';
+import { handlePaddleWebhook, confirmSubscription } from '../controller/subscriptionController.mjs';
 
 
 const router = express.Router();
 
 
 // Webhook endpoint (no authentication required)
-router.post('/webhook', express.raw({ type: 'application/json' }),
- SubscriptionController.handleWebhook);
+router.post('/webhook', handlePaddleWebhook);
 
 
-// Protected routes (require authentication)
-
-
-router.get('/api/subscription/details', SubscriptionController.getSubscriptionDetails);
-// router.get('/api/subscription/payments', SubscriptionController.getPaymentHistory);
-router.post('/api/subscription/cancel', SubscriptionController.cancelSubscription);
+router.get('/api/subscription/confirm', confirmSubscription);
 
 
 export default router;
